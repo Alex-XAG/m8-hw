98a26ad456e5f1e92fc079f352e53e057f7a1308
Oleksii Khomiakov <alex.xag1987@gmail.com>
2023-04-25 00:36:52 +0300

    task2 localStorage done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const PAUSED_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(PAUSED_TIME, seconds);
+}
+
+setCurrentTime();
+
+function setCurrentTime() {
+  if (!localStorage.getItem(PAUSED_TIME)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(PAUSED_TIME));
+}
